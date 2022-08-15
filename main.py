28969ac5c8acacbdb4e6d05454305d60913eca3d
Michal Olowski <olowskimi@outlook.com>
2022-08-15 20:12:38 +0200

    added sys.argv to main execution, form address is easily set in a file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from bot import Bot
+import sys
 
 if __name__ == '__main__':
-    adress = 'https://forms.gle/MkX1C5WE4R3qSh6F6'
-    bot = Bot(adress, 2)
+    forms_n = int(sys.argv[1])
+    adress = ""
+    with open("form_adress.txt", "r") as f:
+        adress = f.read()
+    
+    bot = Bot(adress, forms_n)
     bot.fill_forms()
